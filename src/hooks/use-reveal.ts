7d22ae4c9849c1dc174c459ea76929cb7a0550c2
WattIQs <amaorbelo@gmail.com>
2026-08-12 21:31:55 -0300

    Update use-reveal.ts

--- src/hooks/use-reveal.ts
+++ src/hooks/use-reveal.ts
@@ -22,7 +22,7 @@ export function useReveal<T extends HTMLElement = HTMLDivElement>() {
           }
         }
       },
-      { threshold: 0.15, rootMargin: "0px 0px -60px 0px" },
+      { threshold: 0.08, rootMargin: "0px 0px -12% 0px" },
     );
     observer.observe(node);
     return () => observer.disconnect();
