c2515c64b0eb4a80c1a5c247f889e22649dc5bba
WattIQs <amaorbelo@gmail.com>
2026-08-13 20:34:13 -0300

    Update auth.me.ts\

--- src/routes/auth.me.ts
+++ src/routes/auth.me.ts
@@ -1,27 +1,45 @@
 import { createFileRoute } from "@tanstack/react-router";
+
 import { getSessionUser } from "../lib/session";
 
-export const Route = createFileRoute("/auth/me")({
+export const Route = createFileRoute(
+  "/auth/me",
+)({
   server: {
     handlers: {
       GET: async ({ request }) => {
-        const user = getSessionUser(request);
+        const user =
+          getSessionUser(request);
 
-        console.log("AUTH ME:", {
-          hasCookie: !!request.headers.get("cookie"),
-          authenticated: !!user,
-          user: user
-            ? {
-                email: user.email,
-                sub: user.sub,
-              }
-            : null,
-        });
+        console.log(
+          "AUTH ME:",
+          {
+            hasCookie:
+              !!request.headers.get(
+                "cookie",
+              ),
+
+            authenticated:
+              !!user,
+
+            user: user
+              ? {
+                  email:
+                    user.email,
+                  sub:
+                    user.sub,
+                }
+              : null,
+          },
+        );
 
         return Response.json(
           {
-            authenticated: !!user,
-            user: user ?? null,
+            authenticated:
+              !!user,
+
+            user:
+              user ?? null,
           },
           {
             status: 200,
